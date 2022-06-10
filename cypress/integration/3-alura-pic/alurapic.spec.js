52a68f6a81c0566ef4dbf7898b1c0ba072ae42bf
Samuel Duarte <s.duarte@aluno.ifsp.edu.br>
2022-06-10 02:35:28 -0300

    standardization and good testing practices

--- cypress/integration/3-alura-pic/alurapic.spec.js
+++ cypress/integration/3-alura-pic/alurapic.spec.js
@@ -1,74 +1,30 @@
-describe("login e registro de usuários alura pic", () => {
-  beforeEach(() => {
-    cy.visit("https://alura-fotos.herokuapp.com");
-  });
+describe('Usabilidade tela inicial ', () => {
 
-  it("verifica mensagens validação", () => {
-    cy.contains("a", "Register now").click();
-    cy.contains("button", "Register").click();
-    cy.contains("ap-vmessage", "Email is required").should("be.visible");
-    cy.contains("button", "Register").click();
-    cy.contains("ap-vmessage", "User name is required").should("be.visible");
-    cy.contains("ap-vmessage", "Password is required").should("be.visible");
-    cy.contains("ap-vmessage", "Full name is required").should("be.visible");
-  });
+    beforeEach(() => {
+      cy.visit('https://alura-fotos.herokuapp.com')
+    })
 
-  it("verifica mensagem de email invalido", () => {
-    cy.contains("a", "Register now").click();
-    cy.contains("button", "Register").click();
-    cy.get('input[formcontrolname="email"]').type("jacqueline");
-    cy.contains("ap-vmessage", "Invalid e-mail").should("be.visible");
-  });
+    /* novos casos de teste */
+    it('verifica mensagens tela inicial', () => {
+        cy.contains('ap-vmessage', 'User name is required!').should('be.visible');
+        cy.contains('ap-vmessage', 'Password is required!').should('be.visible');
+        cy.get('button[type="submit"]').should('be.disabled');
+ 
+    })
 
-  it("verifica mensagem de senha com menos de 8 caracteres", () => {
-    cy.contains("a", "Register now").click();
-    cy.contains("button", "Register").click();
-    cy.get('input[formcontrolname="password"]').type("123");
-    cy.contains("button", "Register").click();
-    cy.contains("ap-vmessage", "Mininum length is 8").should("be.visible");
-  });
+    it('verifica botao habilitado na tela inicial', () => {
+        cy.get('input[formcontrolname="userName"]').type('Jacqueline');
+        cy.get('input[formcontrolname="password"]').type('123');
+        cy.get('button[type="submit"]').should('be.enabled');
+    })
 
-  it("verifica mensagem de letra maiuscula no user name", () => {
-    cy.contains("a", "Register now").click();
-    cy.contains("button", "Register").click();
-    cy.get('input[formcontrolname="userName"]').type("Samuel");
-    cy.contains("button", "Register").click();
-    cy.contains("ap-vmessage", "Must be lower case").should("be.visible");
-  });
+    it('verifica nome da aplicação na tela inicial', () => {
+        cy.contains('a' ,' ALURAPIC ').should('be.visible');
+    })
 
-  it("verifica mensagem de minimo de letras no full name", () => {
-    cy.contains("a", "Register now").click();
-    cy.contains("button", "Register").click();
-    cy.get('input[formcontrolname="fullName"]').type("S");
-    cy.contains("button", "Register").click();
-    cy.contains("ap-vmessage", "Mininum length is 2").should("be.visible");
-  });
-
-  it("fazer login de usuario valido", () => {
-    cy.login("flavio", "123");
-    cy.contains("a", "(Logout)").should("be.visible");
-  });
-
-  it("fazer login de usuario invalido", () => {
-    cy.login("jacqueline", "1234");
-    cy.on("windows:alert", (str) => {
-      expect(str).to.equal("Invalid user name or password");
-    });
-  });
-
-  const usuarios = require("../../fixtures/usuarios.json");
-  usuarios.forEach((usuario) => {
-    it.only(`registra novo usuario ${usuario.userName}`, () => {
-      cy.contains("a", "Register now").click();
-      cy.contains("button", "Register").click();
-      cy.get('input[formcontrolname="email"]').type(usuario.email);
-      cy.get('input[formcontrolname="fullName"]').type(usuario.fullName);
-      cy.get('input[formcontrolname="userName"]').type(usuario.userName);
-      cy.get('input[formcontrolname="password"]').type(usuario.password);
-      cy.get('input[formcontrolname="email"]').type(
-        "jacqueline.oliveira@alura.com.br"
-      );
-      cy.contains("button", "Register").click();
-    });
-  });
-});
+    it('verifica menu clicavel tela inicial', () => {
+        cy.get('.navbar-brand > .fa').click();
+        cy.get('.menu-bar > .fa').should('be.visible');
+    })
+    
+})
---
+++ cypress/integration/3-alura-pic/cadastro.spec.js
@@ -0,0 +1,62 @@
+describe("Cadastro de usuários alura pic", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+
+  it("verifica mensagens validação", () => {
+    cy.contains("a", "Register now").click();
+    cy.contains("button", "Register").click();
+    cy.contains("ap-vmessage", "Email is required").should("be.visible");
+    cy.contains("button", "Register").click();
+    cy.contains("ap-vmessage", "User name is required").should("be.visible");
+    cy.contains("ap-vmessage", "Password is required").should("be.visible");
+    cy.contains("ap-vmessage", "Full name is required").should("be.visible");
+  });
+
+  it("verifica mensagem de email invalido", () => {
+    cy.contains("a", "Register now").click();
+    cy.contains("button", "Register").click();
+    cy.get('input[formcontrolname="email"]').type("jacqueline");
+    cy.contains("ap-vmessage", "Invalid e-mail").should("be.visible");
+  });
+
+  it("verifica mensagem de senha com menos de 8 caracteres", () => {
+    cy.contains("a", "Register now").click();
+    cy.contains("button", "Register").click();
+    cy.get('input[formcontrolname="password"]').type("123");
+    cy.contains("button", "Register").click();
+    cy.contains("ap-vmessage", "Mininum length is 8").should("be.visible");
+  });
+
+  it("verifica mensagem de letra maiuscula no user name", () => {
+    cy.contains("a", "Register now").click();
+    cy.contains("button", "Register").click();
+    cy.get('input[formcontrolname="userName"]').type("Samuel");
+    cy.contains("button", "Register").click();
+    cy.contains("ap-vmessage", "Must be lower case").should("be.visible");
+  });
+
+  it("verifica mensagem de minimo de letras no full name", () => {
+    cy.contains("a", "Register now").click();
+    cy.contains("button", "Register").click();
+    cy.get('input[formcontrolname="fullName"]').type("S");
+    cy.contains("button", "Register").click();
+    cy.contains("ap-vmessage", "Mininum length is 2").should("be.visible");
+  });
+
+  const usuarios = require("../../fixtures/usuarios.json");
+  usuarios.forEach((usuario) => {
+    it.only(`registra novo usuario ${usuario.userName}`, () => {
+      cy.contains("a", "Register now").click();
+      cy.contains("button", "Register").click();
+      cy.get('input[formcontrolname="email"]').type(usuario.email);
+      cy.get('input[formcontrolname="fullName"]').type(usuario.fullName);
+      cy.get('input[formcontrolname="userName"]').type(usuario.userName);
+      cy.get('input[formcontrolname="password"]').type(usuario.password);
+      cy.get('input[formcontrolname="email"]').type(
+        "jacqueline.oliveira@alura.com.br"
+      );
+      cy.contains("button", "Register").click();
+    });
+  });
+});
